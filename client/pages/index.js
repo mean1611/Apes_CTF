6b768bd0b4bf232339f857989e8654655c7de976
mean1611 <maen1611@gmail.com>
2023-09-15 01:06:23 +0700

    client

---
+++ client/pages/index.js
@@ -0,0 +1,23 @@
+import React, {useEffect,useState} from "react";
+
+function index() {
+
+  const [message, setMessage] = useState("Loading")
+
+  useEffect(() =>{
+    fetch("http://localhost:8080/api/home").then(
+      response => response.json()
+    ).then(
+      data => {
+        console.log(data)
+        setMessage(data.message)
+      }
+    )
+  }, [])
+
+  return(
+    <div>{message}</div>
+  )
+}
+
+export default index
