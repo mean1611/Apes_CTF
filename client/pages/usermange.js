ba5a4e427a9083454c7cd95a01bd25420a3fbe95
mean1611 <maen1611@gmail.com>
2023-10-26 17:32:53 +0700

    Del_User
    Del User in database form client

--- client/pages/usermange.js
+++ client/pages/usermange.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 function Usermanage() {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
 
-  const getUser = async() => {
-    let result = await axios.get('http://localhost:8080/api/user')
-    setUsers(result.data.data.sort((a, b) => a.user_id - b.user_id))
-    console.log(result.data.data)
+  const getUser = async () => {
+    let result = await axios.get('http://localhost:8080/api/user');
+    setUsers(result.data.data.sort((a, b) => a.user_id - b.user_id));
+    console.log(result.data.data);
   };
 
   useEffect(() => {
-    getUser()
+    getUser();
   }, []);
 
   const handleClickAdd = () => {
@@ -29,7 +30,38 @@ function Usermanage() {
   };
 
   const handleClickDelete = () => {
-    // TODO: ลบผู้ใช้
+    const user_id = selectedUser?.user_id;
+
+    // ตรวจสอบว่า selectedUser มีค่าหรือไม่
+    if (!user_id) {
+      // แสดงข้อความแจ้งเตือน
+      Swal.fire('กรุณาเลือกผู้ใช้ที่ต้องการลบก่อน', '', 'warning');
+      return;
+    }
+
+    Swal.fire({
+      title: 'ต้องการลบข้อมูลผู้ใช้',
+      text: 'ต้องการลบข้อมูลผู้ใช้หรือไม่',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'ใช่, ลบ',
+      cancelButtonText: 'ยกเลิก',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        // ลบผู้ใช้จาก database
+        const response = await axios.delete('http://localhost:8080/api/user/' + user_id);
+
+        if (response.status === 200) {
+          // ลบข้อมูลออกจาก state
+          setSelectedUser(null);
+
+          // อัปเดตรายการผู้ใช้หลังจากลบ
+          setUsers(users.filter(user => user.user_id !== user_id));
+        } else {
+          Swal.fire('เกิดข้อผิดพลาดในการลบข้อมูล', '', 'error');
+        }
+      }
+    });
   };
 
   const handleSubmit = async (e) => {
@@ -72,8 +104,8 @@ function Usermanage() {
                   <td>{user.username}</td>
                   <td>{user.email}</td>
                   <td>
-                    <button className="btn btn-warning" id={user.user_id} onClick={() => handleClickEdit(user)}>แก้ไข</button>
-                    <button className="btn btn-error" id={user.user_id} onClick={handleClickDelete}>ลบ</button>
+                    <button className="btn btn-warning" onClick={() => handleClickEdit(user)}>เลือก</button>
+                    <button className="btn btn-error" onClick={handleClickDelete}>ลบ</button>
                   </td>
                 </tr>
               ))}
@@ -86,11 +118,11 @@ function Usermanage() {
         </div>
         <div className="card-body">
           <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="รหัสผู้ใช้" className="input input-bordered w-full max-w-xs" value={selectedUser?.user_id || ""} />
-            <input type="text" placeholder="อีเมล" className="input input-bordered w-full max-w-xs" value={selectedUser?.email || ""} />
+            <input type="text" placeholder="รหัสผู้ใช้" className="input input-bordered w-full max-w-xs" value={selectedUser?.user_id || ""} readOnly />
+            <input type="text" placeholder="อีเมล" className="input input-bordered w-full max-w-xs" value={selectedUser?.email || ""} readOnly />
             <input type="text" placeholder="ชื่อผู้ใช้" className="input input-bordered w-full max-w-xs" name="username" value={selectedUser?.username || ""} onChange={handleInputChange} />
-            <input type="text" placeholder="Password" className="input input-bordered w-full max-w-xs" name="password"value={selectedUser?.password || ""} onChange={handleInputChange} />
-            <button className="btn btn-success" type="submit">บันทึก</button>
+            <input type="text" placeholder="Password" className="input input-bordered w-full max-w-xs" name="password" value={selectedUser?.password || ""} onChange={handleInputChange} />
+            <button className="btn btn-success ml-2" type="submit">บันทึก</button>
           </form>
         </div>
       </div>
