a7a2351a296356743bf84e81257917fc4cb029f0
ToRShorT <97513475+ToRShorT@users.noreply.github.com>
2023-10-28 14:57:26 +0700

    fix server/sever.js

--- server/server.js
+++ server/server.js
@@ -2,28 +2,10 @@ import express from "express";
 import cors from 'cors';
 
 const app = express();
-<<<<<<< Updated upstream
 const PORT = 8080;
 
 app.get("/", (req , res) => {
     return res.send("HELLO WORLD");
-=======
-
-const cors = require("cors");
-const PORT = 8080;
-
-// const Prisma = require('@prisma/client');
-// const prisma = new Prisma.PrismaClient();
-// const creat = async() => {
-//     const newUser = await prisma.user.
-// }
-
-
-app.use(cors());
-
-app.get("/api/home", (req , res) => {
-    res.json({message: "HELLO WORLD"});
->>>>>>> Stashed changes
 });
 
 // Middleware
@@ -40,9 +22,3 @@ app.use(routes);
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
-<<<<<<< Updated upstream
-=======
-
-
-
->>>>>>> Stashed changes
