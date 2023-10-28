e802cf5e23450bfaad18acf2e2963c468663adad
ToRShorT <97513475+ToRShorT@users.noreply.github.com>
2023-10-28 14:59:53 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,25 @@ const PORT = 8080;
 
 app.get("/", (req , res) => {
     return res.send("HELLO WORLD");
+<<<<<<< Updated upstream
+=======
+=======
+
+const cors = require("cors");
+const PORT = 8080;
+
+// const Prisma = require('@prisma/client');
+// const prisma = new Prisma.PrismaClient();
+// const creat = async() => {
+//     const newUser = await prisma.user.
+// }
+
+
+app.use(cors());
+
+app.get("/api/home", (req , res) => {
+    res.json({message: "HELLO WORLD"});
+>>>>>>> Stashed changes
 });
 
 // Middleware
