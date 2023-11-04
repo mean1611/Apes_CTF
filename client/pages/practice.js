aaf9a5ab7ebbb0729d73ee67dcf8c608c4268ef1
mean1611 <maen1611@gmail.com>
2023-11-05 00:37:02 +0700

    Fix Bug Hint
    Fix Hint always open

--- client/pages/practice.js
+++ client/pages/practice.js
@@ -3,7 +3,6 @@ import Navbaruser from "../components/user/navbarUser.js";
 import Categoryfilter from "../components/user/categoryFilter.js";
 import Swal from 'sweetalert2';
 
-
 function index() {
   const [questions, setQuestions] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(null);
@@ -11,21 +10,17 @@ function index() {
   const [isHintVisible, setIsHintVisible] = useState(false);
   const [answer, setAnswer] = useState("");
   const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState(null); // เพิ่ม state สำหรับเก็บหมวดหมู่ที่เลือก
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-  // ฟังก์ชันสำหรับเปิด Popup เพื่อเล่นคำถาม
   const handlePlayClick = (question) => {
     setCurrentQuestion(question);
     setShowPopup(true);
   };
 
-  // ฟังก์ชันสำหรับแสดงหรือซ่อน Hint
   const handleHintClick = () => {
     setIsHintVisible(!isHintVisible);
   };
 
-
-  // ฟังก์ชันสำหรับส่งคำตอบ
   const handleSubmit = () => {
     if (answer === currentQuestion.answer) {
       Swal.fire({
@@ -47,7 +42,6 @@ function index() {
       .then((response) => response.json())
       .then((data) => {
         if (selectedCategory) {
-          // ถ้ามีหมวดหมู่ที่เลือก กรองโจทย์เฉพาะที่ตรงกับหมวดหมู่นี้
           const filteredQuestions = data.data.filter(
             (question) => question.question_category_id === selectedCategory
           );
@@ -124,36 +118,62 @@ function index() {
       </div>
       {showPopup && currentQuestion && (
         <div className="modal modal-open">
-
           <div className="modal-box  bg-primary-content">
           <div className=" question-content grid grid-rows-9 grid-cols-3">
-            <h3 className="font-bold text-lg  text-primary  row-start-1 col-start-1 col-span-2"> {currentQuestion.question_title}</h3>
-            <p className="text-primary text-primary row-start-2 col-start-1 col-end-3 mt-3">{getCategoryName(currentQuestion.question_category_id)}</p>
-            <p className="text-primary text-primary row-start-2 col-start-3 mt-3">Point : {currentQuestion.score}</p>
+            <h3 className="font-bold text-lg  text-primary  row-start-1 col-start-1 col-span-2">
+              {currentQuestion.question_title}
+            </h3>
+            <p className="text-primary text-primary row-start-2 col-start-1 col-end-3 mt-3">
+              {getCategoryName(currentQuestion.question_category_id)}
+            </p>
+            <p className="text-primary text-primary row-start-2 col-start-3 mt-3">
+              Point : {currentQuestion.score}
+            </p>
             <div className="questionunderline card  row-start-3 col-start-1 col-end-4"></div>
-            <p className="text-primary text-primary row-start-4 col-start-1 col-end-4 mb-2">Description : {currentQuestion.question_desc}</p>
+            <p className="text-primary text-primary row-start-4 col-start-1 col-end-4 mb-2">
+              Description : {currentQuestion.question_desc}
+            </p>
             <div className="questionunderline card  row-start-5 col-start-1 col-end-4"></div>
             <input
-              type="text" 
-              placeholder="Apes{FLAG}" 
+              type="text"
+              placeholder="Apes{FLAG}"
               className="input input-bordered place-items-center mt-2 mb-2 3 text-black row-start-6 col-start-1 col-end-3"
               value={answer}
               onChange={(e) => setAnswer(e.target.value)}
-
             />
-            <button className="submitbutton btn  text-base-100 row-start-6 col-start-3 col-end-4 mt-2 mb-2 " onClick={handleSubmit}>Submit</button>
+            <button
+              className="submitbutton btn  text-base-100 row-start-6 col-start-3 col-end-4 mt-2 mb-2 "
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
             <div className="questionunderline card  row-start-7 col-start-1 col-end-4"></div>
-            <button className="hintbutton btn  text-base-100 mt-2 row-start-8 col-start-1 col-end-2" onClick={handleHintClick}>Hint</button>
-            {isHintVisible && <p className="hint-text text-black row-start-8 col-start-2 col-end-4 ml-2 mt-2 mb-2 ">Hint: {currentQuestion.hint}</p>}
+            <button
+              className="hintbutton btn  text-base-100 mt-2 row-start-8 col-start-1 col-end-2"
+              onClick={handleHintClick}
+            >
+              Hint
+            </button>
+            {isHintVisible && (
+              <p className="hint-text text-black row-start-8 col-start-2 col-end-4 ml-2 mt-2 mb-2 ">
+                Hint: {currentQuestion.hint}
+              </p>
+            )}
             {isAnswerCorrect === true && <p className="text-success">คำตอบถูกต้อง!</p>}
             {isAnswerCorrect === false && <p className="text-error">คำตอบไม่ถูกต้อง</p>}
-            <button className="btn btn-sm btn-circle bg-red-500 text-base-100 absolute right-2 top-2" onClick={() => setShowPopup(false)}>X</button>
-
-            </div>
+            <button
+              className="btn btn-sm btn-circle bg-red-500 text-base-100 absolute right-2 top-2"
+              onClick={() => {
+                setShowPopup(false);
+                setIsHintVisible(false); // ปิด Hint เมื่อปิด Card
+              }}
+            >
+              X
+            </button>
           </div>
-
         </div>
-      )}
+      </div>
+    )}
     </div>
   );
 }
