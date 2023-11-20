3579dfd4063dadf50ea0429bafa5dff9445f3502
ToRShorT <97513475+ToRShorT@users.noreply.github.com>
2023-11-20 16:56:28 +0700

    del_redux
    delete_redux_modules

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from 'react-redux';
 import Navbar from "../components/home/navbar.js";
 import swal from 'sweetalert2';
 
